37a07e753f92679c044f728d79530d6366986850
PC-DEV-01 <pc-dev-01@MacBook-Pro-de-PC-DEV-01.local>
2025-07-29 10:15:53 +0200

    fix: mise à jour apiconfig

--- src/services/apiConfig.ts
+++ src/services/apiConfig.ts
@@ -2,7 +2,7 @@ import axios, { AxiosError, type AxiosResponse, type InternalAxiosRequestConfig 
 import { authService } from './authService';
 
 // URL de base de l'API (aucun proxy requis)
-export const BASE_API_URL = 'https://distritherm-backend.onrender.com';
+export const BASE_API_URL = 'https://distritherm-backend.onrender.com/';
 
 // Types pour la réponse API
 export interface ApiResponse<T> {
